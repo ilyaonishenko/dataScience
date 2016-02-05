ef131ccb71f1d3922cb9bc250c50ad6fb4c7d7d6
wopqw <wopqw@yandex.ru>
2016-02-05 21:59:49 +0300

    no comments

--- decisionTree.py
+++ decisionTree.py
@@ -8,29 +8,13 @@ data = data[data['Age'].notnull()]
 
 data['Sex'][data['Sex']=='female']=0
 data['Sex'][data['Sex']=='male']=1
-# print (data)
-
 
 clf = tree.DecisionTreeClassifier(random_state=241)
 survivers = list(data["Survived"])
 data = data[['Pclass','Fare','Age','Sex']]
 
-
 X = np.array(data)
 Y = np.array(survivers)
 clf=clf.fit(X,Y)
+
 print(clf.feature_importances_)
-
-
-# X = data
-# X=[[1,2],[3,4],[5,6],[7,8],[9,0]]
-# Y=[2,4,6,8,0]?
-# clf = clf.fit(X,Y)
-# dotfile = open('/Users/woqpw/github/datascience/tree.dot','w')
-# dotfile = tree.export_graphviz(clf, out_file = dotfilew)
-# dotfile.close()
-# print (pd.DataFrame(clf.feature_importances_,columns=['Imp']))
-# print(clf.feature_importances_)
-# print(data)
-# print("___________")
-# print(Y)
